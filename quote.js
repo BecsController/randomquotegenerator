b5a10324f07ee6922e1b1c7395013c9c0c93b80f
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-03-28 21:06:36 +1300

    color change wip

--- quote.js
+++ quote.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start() {
+
 var reset = document.getElementById("new");
 
-addEventListener("click", newQuote);
+reset.addEventListener("click", newQuote);
 
+function newQuote (){
+var backgrounds = document.getElementsByClassName("color").style.backgroundColor;
+var colors = ['#669999','	#669973','#667399','#996699','#996666','#cc0000','#66cc00','#00cccc'];
+
+backgrounds = colors[Math.floor(Math.random() * colors.length)];
+
+}
 
 }
