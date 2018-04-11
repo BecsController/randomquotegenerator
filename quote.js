2cc76214a2d4fcb875e456c3d28f7f0dbaba2f4f
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-04-12 11:57:23 +1200

    quote array change

--- quote.js
+++ quote.js
@@ -2,26 +2,16 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start() {
   var whatPeopleSay = [
-    'Injustice anywhere is a threat to justice everywhere.',
-    'The problem with the world is that the intelligent people are full of doubts while the stupid ones are full of confidence.',
-    'A feminist is any woman who tells the truth about her life.',
-    'Be yourself; everyone else is already taken.',
-    'You only live once, but if you do it right, once is enough.',
-    'Ive learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.',
-    'Without music, life would be a mistake.',
-    'Tonight I can write the saddest lines, I loved her, and sometimes she loved me too.'
+    ['Injustice anywhere is a threat to justice everywhere.', 'Martin Luther King, Jr.'],
+    ['The problem with the world is that the intelligent people are full of doubts while the stupid ones are full of confidence.', 'Charles Bukowski'],
+    ['A feminist is any woman who tells the truth about her life.', 'Virginia Woolf'],
+    ['Be yourself; everyone else is already taken.', 'Oscar Wilde'],
+    ['You only live once, but if you do it right, once is enough.', 'Mae West'],
+    ['Ive learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.', 'Maya Angelou'],
+    ['Without music, life would be a mistake.', 'Friedrich Nietzsche'],
+    ['Tonight I can write the saddest lines, I loved her, and sometimes she loved me too.', 'Pablo Neruda']
   ];
 
-  var thePeopleWhoSayThem= [
-    'Martin Luther King, Jr.',
-    'Charles Bukowski',
-    'Virginia Woolf',
-    'Oscar Wilde',
-    'Mae West',
-    'Maya Angelou',
-    'Friedrich Nietzsche',
-    'Pablo Neruda'
-  ];
 var reset = document.getElementById("new");
 var colors = ['#669999','#669973','#667399','#996699','#996666','#cc0000','#66cc00','#00cccc'];
 var newAuthor = document.getElementsByClassName("author")[0];
@@ -30,8 +20,9 @@ var newQuotes = document.getElementsByClassName("quote")[0];
 reset.addEventListener("click", newQuote);
 
 function newQuote (){
-newQuotes.innerHTML = whatPeopleSay[Math.floor(Math.random() * 8)];
-newAuthor.innerHTML = "Charles Bukowski";
+var createNewQuote = whatPeopleSay[Math.floor(Math.random() * 8)];
+newQuotes.innerHTML = createNewQuote[0];
+newAuthor.innerHTML = createNewQuote[1];
 document.getElementsByClassName("color").style.backgroundColor = colors[Math.floor(Math.random() * 8)];
   }
 }
