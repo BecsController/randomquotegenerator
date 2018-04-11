d06101f84d34f54f7c3ce77e2805726f63129552
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-04-12 11:27:57 +1200

    click quote change functions

--- quote.js
+++ quote.js
@@ -3,15 +3,15 @@ document.addEventListener('DOMContentLoaded', start)
 function start() {
 
 var reset = document.getElementById("new");
+var colors = ['#669999','#669973','#667399','#996699','#996666','#cc0000','#66cc00','#00cccc'];
+var newAuthor = document.getElementsByClassName("author")[0];
+var newQuotes = document.getElementsByClassName("quote")[0];
 
 reset.addEventListener("click", newQuote);
 
 function newQuote (){
-var backgrounds = document.getElementsByClassName("color").style.backgroundColor;
-var colors = ['#669999','	#669973','#667399','#996699','#996666','#cc0000','#66cc00','#00cccc'];
-
-backgrounds = colors[Math.floor(Math.random() * colors.length)];
-
-}
-
+newQuotes.innerHTML = "The problem with the world is that the intelligent people are full of doubts while the stupid ones are full of confidence.";
+newAuthor.innerHTML = "Charles Bukowski";
+document.getElementsByClassName("color").style.backgroundColor = colors[Math.floor(Math.random() * 8)];
+  }
 }
